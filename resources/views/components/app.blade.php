5288a2b1df6ce84541aec6634a5d79bdfab115b8
Chamodya Wimansha <chamodyawimansha@gmail.com>
2021-04-18 14:10:09 +0530

    changes the app name

--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <title>Url Re-router</title>
+    <title>{{config('app.name')}}</title>
 </head>
 <body>
     @yield('content')
